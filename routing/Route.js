e303a5bf53ea9f04458e8d6b085c8dfb56bcb7d1
Mehrab Ali <mehrabalishah52@gmail.com>
2024-02-01 20:07:49 +0500

    archetecture done

---
+++ routing/Route.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Route = () => {
+  return (
+    <View>
+      <Text>route</Text>
+    </View>
+  )
+}
+
+export default Route
