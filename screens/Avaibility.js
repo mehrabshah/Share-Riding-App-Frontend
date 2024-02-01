baa0bb05ad7a1ff4782bdc5ce50a05a1eba9cf60
Mehrab Ali <mehrabalishah52@gmail.com>
2024-02-01 20:54:40 +0500

    complete architecture done

---
+++ screens/Avaibility.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+const Avaibility = () => {
+  return (
+    <View>
+      <Text>Avaibility</Text>
+    </View>
+  )
+}
+
+export default Avaibility
+
+const styles = StyleSheet.create({})
